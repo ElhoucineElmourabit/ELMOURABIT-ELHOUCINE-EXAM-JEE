3072ae7eef00bf6f70e85bc6710da08b1a257a94
ElhoucineElmourabit <elhoucine.elmourabit.pro@gmail.com>
2025-05-19 08:57:16 +0100

    init commit

---
+++ elhoucine/src/main/java/bdcc/elmourabit/elhoucine/ElhoucineApplication.java
@@ -0,0 +1,13 @@
+package bdcc.elmourabit.elhoucine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElhoucineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElhoucineApplication.class, args);
+	}
+
+}
---
+++ elhoucine/src/test/java/bdcc/elmourabit/elhoucine/ElhoucineApplicationTests.java
@@ -0,0 +1,13 @@
+package bdcc.elmourabit.elhoucine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElhoucineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
